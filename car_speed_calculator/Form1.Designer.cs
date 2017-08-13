b911bffc051082e0110d3469b0a9aebf0ec434c6
Sharan Khan <sharan.gohar@gmail.com>
2017-08-13 15:17:41 +0500

    major ui updates

--- car_speed_calculator/Form1.Designer.cs
+++ car_speed_calculator/Form1.Designer.cs
@@ -31,14 +31,7 @@
             this.button_start = new System.Windows.Forms.Button();
             this.button_select_video = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
-            this.pictureBox1Label = new System.Windows.Forms.Label();
-            this.pictureBox2Label = new System.Windows.Forms.Label();
-            this.pictureBox3Label = new System.Windows.Forms.Label();
-            this.pictureBox4Label = new System.Windows.Forms.Label();
             this.labelErosionTitle = new System.Windows.Forms.Label();
             this.labelErosionValue = new System.Windows.Forms.Label();
             this.buttonErosionPlus = new System.Windows.Forms.Button();
@@ -47,9 +40,6 @@
             this.labelDilationValue = new System.Windows.Forms.Label();
             this.buttonDilationPlus = new System.Windows.Forms.Button();
             this.buttonDilationMinus = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             this.SuspendLayout();
             // 
@@ -82,73 +72,13 @@
             this.openFileDialog1.FileName = "openFileDialog1";
             this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(450, 300);
-            this.pictureBox1.TabIndex = 13;
-            this.pictureBox1.TabStop = false;
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Location = new System.Drawing.Point(587, 12);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(450, 300);
-            this.pictureBox2.TabIndex = 14;
-            this.pictureBox2.TabStop = false;
-            // 
-            // pictureBox3
-            // 
-            this.pictureBox3.Location = new System.Drawing.Point(12, 354);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(450, 300);
-            this.pictureBox3.TabIndex = 15;
-            this.pictureBox3.TabStop = false;
-            // 
             // pictureBox4
             // 
-            this.pictureBox4.Location = new System.Drawing.Point(587, 354);
+            this.pictureBox4.Location = new System.Drawing.Point(23, 12);
             this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(450, 300);
+            this.pictureBox4.Size = new System.Drawing.Size(1012, 689);
             this.pictureBox4.TabIndex = 16;
             this.pictureBox4.TabStop = false;
-            // 
-            // pictureBox1Label
-            // 
-            this.pictureBox1Label.AutoSize = true;
-            this.pictureBox1Label.Location = new System.Drawing.Point(155, 325);
-            this.pictureBox1Label.Name = "pictureBox1Label";
-            this.pictureBox1Label.Size = new System.Drawing.Size(72, 13);
-            this.pictureBox1Label.TabIndex = 17;
-            this.pictureBox1Label.Text = "Orignal Image";
-            // 
-            // pictureBox2Label
-            // 
-            this.pictureBox2Label.AutoSize = true;
-            this.pictureBox2Label.Location = new System.Drawing.Point(761, 325);
-            this.pictureBox2Label.Name = "pictureBox2Label";
-            this.pictureBox2Label.Size = new System.Drawing.Size(82, 13);
-            this.pictureBox2Label.TabIndex = 18;
-            this.pictureBox2Label.Text = "Binarized Image";
-            // 
-            // pictureBox3Label
-            // 
-            this.pictureBox3Label.AutoSize = true;
-            this.pictureBox3Label.Location = new System.Drawing.Point(155, 668);
-            this.pictureBox3Label.Name = "pictureBox3Label";
-            this.pictureBox3Label.Size = new System.Drawing.Size(129, 13);
-            this.pictureBox3Label.TabIndex = 19;
-            this.pictureBox3Label.Text = "Applying canny opertation";
-            // 
-            // pictureBox4Label
-            // 
-            this.pictureBox4Label.AutoSize = true;
-            this.pictureBox4Label.Location = new System.Drawing.Point(761, 668);
-            this.pictureBox4Label.Name = "pictureBox4Label";
-            this.pictureBox4Label.Size = new System.Drawing.Size(37, 13);
-            this.pictureBox4Label.TabIndex = 20;
-            this.pictureBox4Label.Text = "Result";
             // 
             // labelErosionTitle
             // 
@@ -241,20 +171,10 @@
             this.Controls.Add(this.buttonErosionPlus);
             this.Controls.Add(this.labelErosionValue);
             this.Controls.Add(this.labelErosionTitle);
-            this.Controls.Add(this.pictureBox4Label);
-            this.Controls.Add(this.pictureBox3Label);
-            this.Controls.Add(this.pictureBox2Label);
-            this.Controls.Add(this.pictureBox1Label);
             this.Controls.Add(this.pictureBox4);
-            this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button_select_video);
             this.Controls.Add(this.button_start);
             this.Name = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -269,14 +189,7 @@
         private System.Windows.Forms.Button button_start;
         private System.Windows.Forms.Button button_select_video;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.PictureBox pictureBox4;
-        private System.Windows.Forms.Label pictureBox1Label;
-        private System.Windows.Forms.Label pictureBox2Label;
-        private System.Windows.Forms.Label pictureBox3Label;
-        private System.Windows.Forms.Label pictureBox4Label;
         private System.Windows.Forms.Label labelErosionTitle;
         private System.Windows.Forms.Label labelErosionValue;
         private System.Windows.Forms.Button buttonErosionPlus;
--- car_speed_calculator/Form1.cs
+++ car_speed_calculator/Form1.cs
@@ -31,9 +31,9 @@ namespace car_speed_calculator
         public Form1()
         {
             InitializeComponent();
-            pictureBox1.ImageLocation = "placeholder-image.jpg";
-            pictureBox2.ImageLocation = "placeholder-image.jpg";
-            pictureBox3.ImageLocation = "placeholder-image.jpg";
+            //pictureBox1.ImageLocation = "placeholder-image.jpg";
+            //pictureBox2.ImageLocation = "placeholder-image.jpg";
+            //pictureBox3.ImageLocation = "placeholder-image.jpg";
             pictureBox4.ImageLocation = "placeholder-image.jpg";
         }
         private void My_Timer_Tick(object sender, EventArgs e)
@@ -49,30 +49,30 @@ namespace car_speed_calculator
                 int erosionValue = Convert.ToInt32(labelErosionValue.Text.ToString());
                 int dilationValue = Convert.ToInt32(labelDilationValue.Text.ToString());
                 #endregion
-                pictureBox1.Image = orignalFrame.ToBitmap();
+                pictureBox4.Image = orignalFrame.ToBitmap();
                 //convert image into gray
-                Image<Bgr, byte> image = orignalFrame.Resize(pictureBox1.Width, pictureBox1.Height, 0);
+                Image<Bgr, byte> image = orignalFrame.Resize(pictureBox4.Width, pictureBox4.Height, 0);
                 Image<Gray, byte> frame = image.Convert<Gray, byte>();
                 //perform differencing on them to find the 'new introduction to the background and motions"
-                Image<Gray, byte> BgDifference = new Image<Gray, byte>(pictureBox1.Width, pictureBox1.Height);
-                Image<Gray, byte> FrameDifference = new Image<Gray, byte>(pictureBox1.Width, pictureBox1.Height);
+                Image<Gray, byte> BgDifference = new Image<Gray, byte>(pictureBox4.Width, pictureBox4.Height);
+                Image<Gray, byte> FrameDifference = new Image<Gray, byte>(pictureBox4.Width, pictureBox4.Height);
                 //since i don't have a background image let next frame be considered as background
                 if(_capture.QueryFrame().ToImage<Bgr,byte>() == null)
                 {
                     My_Time.Stop();
                     return;
                 }
-                Image<Bgr, byte> nextFrame = _capture.QueryFrame().ToImage<Bgr, byte>().Resize(pictureBox1.Width,pictureBox1.Height,0);
+                Image<Bgr, byte> nextFrame = _capture.QueryFrame().ToImage<Bgr, byte>().Resize(pictureBox4.Width,pictureBox4.Height,0);
                 Image<Gray, byte> nextFrameGray = nextFrame.Convert<Gray, byte>();
                 CvInvoke.AbsDiff(frame, nextFrameGray, BgDifference);
                 //Perform thresholding to remove noise and boost "new introductions"
-                Image<Gray, byte> thresholded = new Image<Gray, byte>(pictureBox1.Width, pictureBox1.Height);
+                Image<Gray, byte> thresholded = new Image<Gray, byte>(pictureBox4.Width, pictureBox4.Height);
                 CvInvoke.Threshold(BgDifference, thresholded, 20, 255, Emgu.CV.CvEnum.ThresholdType.Binary);
-                pictureBox2.Image = thresholded.Bitmap;
-                pictureBox2Label.Text = "Abs Difference image";
+                //pictureBox2.Image = thresholded.Bitmap;
+                //pictureBox2Label.Text = "Abs Difference image";
                 //Perform erosion to remove camera noise
                 var element = CvInvoke.GetStructuringElement(Emgu.CV.CvEnum.ElementShape.Rectangle, new Size(2, 2), new Point(-1, -1));
-                Image<Gray, byte> eroded = new Image<Gray, byte>(pictureBox1.Width, pictureBox1.Height);
+                Image<Gray, byte> eroded = new Image<Gray, byte>(pictureBox4.Width, pictureBox4.Height);
                 if(isDilated)
                     CvInvoke.Dilate(thresholded, eroded, element, new Point(-1, -1), dilationValue, Emgu.CV.CvEnum.BorderType.Default, default(MCvScalar));
                 if(isEroded)
@@ -80,8 +80,8 @@ namespace car_speed_calculator
                 System.Drawing.Size kSize = new Size(10, 10);
                 CvInvoke.Blur(thresholded, eroded, kSize,new Point(-1,-1),Emgu.CV.CvEnum.BorderType.Default);
                 CvInvoke.Threshold(eroded, eroded, 10, 255, Emgu.CV.CvEnum.ThresholdType.Binary);
-                pictureBox3.Image = eroded.Bitmap;
-                pictureBox3Label.Text = "Eroded/Dilated Image";
+                //pictureBox3.Image = eroded.Bitmap;
+                //pictureBox3Label.Text = "Eroded/Dilated Image";
                 //Takes the threshholded image and looks for square and draws the squares out on top of the current frame
                 drawBoxes(eroded, image);
             }
@@ -93,7 +93,7 @@ namespace car_speed_calculator
             Gray circleAccumulatorThreshhold = new Gray(120);
 
             Image<Gray, byte> cannyEdges = img.Canny(180,120);
-            pictureBox3.Image = cannyEdges.ToBitmap();
+            //pictureBox3.Image = cannyEdges.ToBitmap();
             //LineSegment2D[] lines = cannyEdges.HoughLinesBinary(
             //    1,//Distance resolution in pixel-related units
             //    Math.PI / 45.0,//Angle resolution measured in radians
@@ -207,12 +207,12 @@ namespace car_speed_calculator
                 }
                 VideoCapture previewCapture = new VideoCapture(file);
                 this.firstFrame = previewCapture.QueryFrame().ToImage<Bgr, Byte>();
-                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
-                pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
-                pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+                //pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                //pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+                //pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
                 pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
                 this.firstFrameGray = this.firstFrame.Convert<Gray, byte>();
-                pictureBox1.Image = this.firstFrame.ToBitmap();
+                //pictureBox1.Image = this.firstFrame.ToBitmap();
 
             }
         }
