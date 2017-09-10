0c5993fecaccaf67f1bdeeb0178b1fa529d1c4e3
Sharan <sharan.gohar@gmail.com>
2017-09-10 23:26:11 +0500

    Added Speed detector

--- car_speed_calculator/Form1.Designer.cs
+++ car_speed_calculator/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace car_speed_calculator
+namespace car_speed_calculator
 {
     partial class Form1
     {
@@ -40,6 +40,14 @@
             this.labelDilationValue = new System.Windows.Forms.Label();
             this.buttonDilationPlus = new System.Windows.Forms.Button();
             this.buttonDilationMinus = new System.Windows.Forms.Button();
+            this.labelSpeed = new System.Windows.Forms.Label();
+            this.labelSpeedValue = new System.Windows.Forms.Label();
+            this.labelkmTitle = new System.Windows.Forms.Label();
+            this.buttonUp = new System.Windows.Forms.Button();
+            this.labelRectangleAdj = new System.Windows.Forms.Label();
+            this.buttonDown = new System.Windows.Forms.Button();
+            this.buttonLeft = new System.Windows.Forms.Button();
+            this.buttonRight = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             this.SuspendLayout();
             // 
@@ -160,9 +168,98 @@
             this.buttonDilationMinus.UseVisualStyleBackColor = true;
             this.buttonDilationMinus.Click += new System.EventHandler(this.buttonDilationMinus_Click);
             // 
+            // labelSpeed
+            // 
+            this.labelSpeed.AutoSize = true;
+            this.labelSpeed.Font = new System.Drawing.Font("Microsoft Sans Serif", 23F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSpeed.Location = new System.Drawing.Point(1074, 350);
+            this.labelSpeed.Name = "labelSpeed";
+            this.labelSpeed.Size = new System.Drawing.Size(104, 35);
+            this.labelSpeed.TabIndex = 29;
+            this.labelSpeed.Text = "Speed";
+            this.labelSpeed.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // labelSpeedValue
+            // 
+            this.labelSpeedValue.AutoSize = true;
+            this.labelSpeedValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSpeedValue.Location = new System.Drawing.Point(1074, 409);
+            this.labelSpeedValue.Name = "labelSpeedValue";
+            this.labelSpeedValue.Size = new System.Drawing.Size(29, 31);
+            this.labelSpeedValue.TabIndex = 30;
+            this.labelSpeedValue.Text = "0";
+            // 
+            // labelkmTitle
+            // 
+            this.labelkmTitle.AutoSize = true;
+            this.labelkmTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelkmTitle.Location = new System.Drawing.Point(1128, 415);
+            this.labelkmTitle.Name = "labelkmTitle";
+            this.labelkmTitle.Size = new System.Drawing.Size(61, 25);
+            this.labelkmTitle.TabIndex = 31;
+            this.labelkmTitle.Text = "km/hr";
+            // 
+            // buttonUp
+            // 
+            this.buttonUp.Location = new System.Drawing.Point(1094, 524);
+            this.buttonUp.Name = "buttonUp";
+            this.buttonUp.Size = new System.Drawing.Size(60, 23);
+            this.buttonUp.TabIndex = 32;
+            this.buttonUp.Text = "UP";
+            this.buttonUp.UseVisualStyleBackColor = true;
+            this.buttonUp.Click += new System.EventHandler(this.buttonUp_Click);
+            // 
+            // labelRectangleAdj
+            // 
+            this.labelRectangleAdj.AutoSize = true;
+            this.labelRectangleAdj.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelRectangleAdj.Location = new System.Drawing.Point(1041, 475);
+            this.labelRectangleAdj.Name = "labelRectangleAdj";
+            this.labelRectangleAdj.Size = new System.Drawing.Size(169, 22);
+            this.labelRectangleAdj.TabIndex = 33;
+            this.labelRectangleAdj.Text = "Speed Box Adjustor";
+            // 
+            // buttonDown
+            // 
+            this.buttonDown.Location = new System.Drawing.Point(1094, 571);
+            this.buttonDown.Name = "buttonDown";
+            this.buttonDown.Size = new System.Drawing.Size(60, 23);
+            this.buttonDown.TabIndex = 34;
+            this.buttonDown.Text = "DOWN";
+            this.buttonDown.UseVisualStyleBackColor = true;
+            this.buttonDown.Click += new System.EventHandler(this.buttonDown_Click);
+            // 
+            // buttonLeft
+            // 
+            this.buttonLeft.Location = new System.Drawing.Point(1041, 542);
+            this.buttonLeft.Name = "buttonLeft";
+            this.buttonLeft.Size = new System.Drawing.Size(46, 23);
+            this.buttonLeft.TabIndex = 35;
+            this.buttonLeft.Text = "LEFT";
+            this.buttonLeft.UseVisualStyleBackColor = true;
+            this.buttonLeft.Click += new System.EventHandler(this.buttonLeft_Click);
+            // 
+            // buttonRight
+            // 
+            this.buttonRight.Location = new System.Drawing.Point(1160, 542);
+            this.buttonRight.Name = "buttonRight";
+            this.buttonRight.Size = new System.Drawing.Size(51, 23);
+            this.buttonRight.TabIndex = 36;
+            this.buttonRight.Text = "RIGHT";
+            this.buttonRight.UseVisualStyleBackColor = true;
+            this.buttonRight.Click += new System.EventHandler(this.buttonRight_Click);
+            // 
             // Form1
             // 
             this.ClientSize = new System.Drawing.Size(1219, 733);
+            this.Controls.Add(this.buttonRight);
+            this.Controls.Add(this.buttonLeft);
+            this.Controls.Add(this.buttonDown);
+            this.Controls.Add(this.labelRectangleAdj);
+            this.Controls.Add(this.buttonUp);
+            this.Controls.Add(this.labelkmTitle);
+            this.Controls.Add(this.labelSpeedValue);
+            this.Controls.Add(this.labelSpeed);
             this.Controls.Add(this.buttonDilationMinus);
             this.Controls.Add(this.buttonDilationPlus);
             this.Controls.Add(this.labelDilationValue);
@@ -198,6 +295,14 @@
         private System.Windows.Forms.Label labelDilationValue;
         private System.Windows.Forms.Button buttonDilationPlus;
         private System.Windows.Forms.Button buttonDilationMinus;
+        private System.Windows.Forms.Label labelSpeed;
+        private System.Windows.Forms.Label labelSpeedValue;
+        private System.Windows.Forms.Label labelkmTitle;
+        private System.Windows.Forms.Button buttonUp;
+        private System.Windows.Forms.Label labelRectangleAdj;
+        private System.Windows.Forms.Button buttonDown;
+        private System.Windows.Forms.Button buttonLeft;
+        private System.Windows.Forms.Button buttonRight;
     }
 }
 
